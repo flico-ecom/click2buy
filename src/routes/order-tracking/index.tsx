69f764e37a17b672b81c1c53aa9eb581b70fbe7d
dev-amila <amilaprasadbandara1994@gmail.com>
2025-07-14 14:40:43 +0530

    make mobile responsive tracking page

--- src/routes/order-tracking/index.tsx
+++ src/routes/order-tracking/index.tsx
@@ -22,16 +22,16 @@ export default component$(() => {
 	});
 
 	return (
-		<div class="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+		<div class="flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8">
 			<div class="sm:mx-auto sm:w-full sm:max-w-md">
-				<h2 class="mt-6 text-center text-3xl text-gray-900">Track Your Order</h2>
-				<p class="mt-2 text-center text-sm text-gray-600">
+				<h2 class="mt-6 text-center text-3xl title">Track Your Order</h2>
+				<p class="mt-2 text-center text-sm text-gray-600 sub-title">
 					Enter your tracking number to check the status of your order.
 				</p>
 			</div>
 
 			<div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-				<div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+				<div class="bg-white py-8 px-4 shadow rounded-lg sm:px-10">
 					<div class="space-y-6">
 						<div>
 							<label class="block text-sm font-medium text-gray-700">Tracking Number</label>
